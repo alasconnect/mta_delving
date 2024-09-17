898cc12b6759b33481ad600d8a4f1840a07a3133
Edward Lionte <eddonomochi@gmail.com>
2024-09-17 16:52:05 -0400

    Revert "init push"
    This reverts commit 20487bc5a6fa0d1d6975fcc4687cc059307180f7.

--- src/Delving.API.Core/Library.fs
+++
@@ -1,7 +0,0 @@
-namespace Delving.API.Core.Library
-
-open Delving.API.Core
-
-[<AutoOpen>]
-module Default =
-    let PLACEHOLDER (services : IServices) : Async<unit> = async { return () }
--- src/Delving.API/Services/LEBStore.fs
+++
@@ -1,8 +0,0 @@
-module Delving.API.Services.LEBStore
-
-open Delving.API.Core
-open Delving.API.DataAccess
-
-type DefaultLEBStore (lebDb : LineEquipmentBackupDbContext) =
-    interface ILEBStore with
-        member _.PLACEHOLDER () : Async<unit> = async { return () }
